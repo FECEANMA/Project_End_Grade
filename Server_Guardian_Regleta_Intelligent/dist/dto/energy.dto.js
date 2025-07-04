6c94c054d80020fc8e0ebb35cfe2d2288e0236c5
Fernando <146852179+FECEANMA@users.noreply.github.com>
2025-07-03 23:43:55 -0500

    Add files via upload

---
+++ Server_Guardian_Regleta_Intelligent/dist/dto/energy.dto.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EnergyDto = void 0;
+class EnergyDto {
+    voltage;
+    current;
+    power;
+}
+exports.EnergyDto = EnergyDto;
+//# sourceMappingURL=energy.dto.js.map
